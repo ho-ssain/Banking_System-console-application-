d9c4c45718898f85c11340b8864f8d8c909e55d2
Ahnaf-Tahmid-009 <59658472+Ahnaf-Tahmid-009@users.noreply.github.com>
2020-05-15 04:52:32 +0600

    Add files via upload
    Adding class "Account"

---
+++ Account.java
@@ -0,0 +1,29 @@
+package Banking_System;
+
+abstract class Account{
+	String name;
+	float balance;
+	int account_num;
+	static float interest = (float) 3.75f;
+	
+	
+	Account(String name , float balance, int account_num){
+		this.name = name;
+		this.balance = balance;
+		this.account_num = account_num;
+	}
+
+	abstract float deposite(int amount);
+
+	abstract void withdraw(int amount);
+
+	abstract void transfer(int amount);
+
+	abstract void changeInterest(float interest);
+
+	abstract void addInterest(int year);
+
+	abstract void showAccount();
+
+	
+}
